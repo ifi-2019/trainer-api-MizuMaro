af6db46c010d5fb0012a5ff10c7a1e41f3ae4b33
Al Yasini Omar <alyasini@b11p10.fil.univ-lille1.fr>
2019-11-18 11:28:33 +0100

    update

--- src/main/java/com/ifi/trainer_api/controller/TrainerController.java
+++ src/main/java/com/ifi/trainer_api/controller/TrainerController.java
@@ -3,10 +3,7 @@ package com.ifi.trainer_api.controller;
 import com.ifi.trainer_api.bo.Trainer;
 import com.ifi.trainer_api.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trainers")
@@ -24,6 +21,10 @@ public class TrainerController {
     @GetMapping("/{name}")
     Trainer getTrainer(@PathVariable String name){
         return trainerService.getTrainer(name);
+    }
+    @PostMapping("/" )
+    Trainer addTrainer(@RequestBody Trainer t){
+        return trainerService.createTrainer(t);
     }
 
 }
